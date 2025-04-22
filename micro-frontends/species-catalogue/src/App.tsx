e32368e6c4f6ab16bd85cd2a2cc3e6c92bddfe86
Shubham Khandelwal <shubhamespn@gmail.com>
2025-04-22 13:51:51 +0530

    SK01: adding provider and other ui tools

--- micro-frontends/species-catalogue/src/App.tsx
+++ micro-frontends/species-catalogue/src/App.tsx
@@ -1,21 +1,18 @@
-import './App.css'
-import SpeciesCatalogue from './components/SpeciesCatalogue.tsx'
-import { Provider } from '../../../src/components/ui/provider.tsx';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
+import "./App.css";
+import SpeciesCatalogue from "./components/SpeciesCatalogue.tsx";
+import { Provider } from "../src/components/ui/provider";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <Provider>
-      {/* <SpeciesCatalogue/> */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<SpeciesCatalogue />} />
         </Routes>
       </BrowserRouter>
     </Provider>
-  )
+  );
 }
 
-export default App
+export default App;
